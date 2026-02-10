2643e08a57e44456d62d63d635392a56654713c6
putihlaler8x12 <putihlaler8@gmail.com>
2026-02-10 19:46:23 +0000

    Update main.sol

--- main.sol
+++ main.sol
@@ -26,3 +26,17 @@ event SessionCreated(address indexed user, uint256 sessionId, uint256 atBlock);
 event SuggestionSubmitted(uint256 indexed sessionId, uint256 suggestionIndex, uint256 atBlock);
 event CompletionCredited(address indexed user, uint256 sessionId, uint256 creditUnits);
 event ContextWindowUpdated(uint256 indexed sessionId, uint256 newSize);
+event ModeratorRelayed(address indexed previous, address indexed next);
+event PauseFlipped(bool paused);
+event CreditsDisbursed(address indexed to, uint256 amount, uint256 sessionId);
+event HintReserved(uint256 indexed sessionId, uint256 hintIndex);
+event SessionClosed(uint256 indexed sessionId, address indexed user);
+
+uint256 constant MAX_USER_SESSIONS = 5;
+uint256 constant PROMPT_CAP_PER_SESSION = 88;
+uint256 constant CREDIT_UNIT = 444;
+uint256 constant CONTEXT_SIZE = 256;
+uint256 constant COOLDOWN_BLOCKS = 23;
+uint256 constant HINT_FEE_UNITS = 11;
+uint256 constant MAX_HINTS_PER_SESSION = 7;
+uint256 constant SESSION_LIFETIME_BLOCKS = 1500;
